99928684fe72ecc4afb8da5d434ab24a75c61357
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2022-03-25 14:27:31 +1300

    Name-Number/coded fav_num-(double, half, squared)

--- 03_getting_input.py
+++ 03_getting_input.py
@@ -2,11 +2,20 @@
 username = input("what is your name? ")
 
 # Ask the user for their favourite integer
-fav_num = int(input("Favourite Number? ")
+fav_num = int(input("Favourite Number? "))
 
 # Double, halve and square the number
+double = fav_num * 2
+half = fav_num / 2 
+squared = fav_num * fav_num
 
+print()
 # Greet the user
-
-# Output the results of doubling, halving
-# and squaring their favourite number
+print("Hi {}, you favourite number is {}". format(username, fav_num))
+print()
+# Output the results of doubling, halving 
+# and squaring their faourite  number  
+print("double {} is {}". format(fav_num, double))
+print("half {} is {}". format(fav_num, half))
+print("{} squared is {}". format(fav_num, squared))
+print()
